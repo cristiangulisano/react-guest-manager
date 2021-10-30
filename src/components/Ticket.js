b3208d4f1507d0139c7400fca536927d8193a632
A308844 <cgulisanoaraujo@santandertecnologia.com.ar>
2021-10-29 22:33:54 -0300

    refactor

---
+++ src/components/Ticket.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+
+class Ticket extends Component {
+
+    render() {
+        return (
+            <form onSubmit={this.initilize}>
+                    
+                    <figure class="text-center">
+                        <blockquote class="blockquote">
+                            <p class="mb-0">Gracias por confirmar!</p>
+                            <p class="mb-0">A continuación podes descargar tu invitación</p>
+                            <p class="mb-0">Te esperamos!!</p>
+                            <p></p>
+                        </blockquote>             
+                    </figure>
+
+                    <div class="card border-dark mb-3" styles="max-width: 20rem;">
+                        <div class="card-header">Love Airlines</div>
+                        <div class="card-body">
+                        <div className="row">
+                            <div className="col-lg-6">
+                                <div className="bs-component">
+                                <div class="card border-dark mb-3" styles="max-width: 20rem;">
+                                <div class="card-body">
+                                    <h8 class="card-title">Pasajero</h8>
+                                    <h6 class="card-text">Pepe Argento</h6>
+                                </div>
+                                </div>
+                                </div>
+                            </div>
+                            <div className="col-lg-6">
+                                <div className="bs-component">
+                                <div class="card border-dark mb-3" styles="max-width: 20rem;">
+                                <div class="card-body">
+                                    <h8 class="card-title">Partida</h8>
+                                    <h6 class="card-text">Buenos Aires, Puerto Madero</h6>
+                                    <h8 class="card-title">Destino</h8>
+                                    <h6 class="card-text">Forever</h6>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                        </div>
+                    </div>
+
+
+                </form>
+          );
+    }
+}
+export default Ticket;
