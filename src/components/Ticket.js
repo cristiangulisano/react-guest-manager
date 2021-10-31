2f67f88e7009132dfc7f4df42aa62a36885a6320
A308844 <cgulisanoaraujo@santandertecnologia.com.ar>
2021-10-31 13:18:08 -0300

    refactor

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -35,28 +35,40 @@ class Ticket extends Component {
                         <div class="card-header">Love Airlines</div>
                         <div class="card-body">
                         <div className="row">
-                            <div className="col-lg-6">
+                            <div className="col-lg-4">
                                 <div className="bs-component">
                                 <div class="card border-dark mb-3" styles="max-width: 20rem;">
                                 <div class="card-body">
                                     <h8 class="card-title">Pasajero</h8>
                                     <h6 class="card-text">{this.props.mensaje.name}</h6>
+                                    <h8 class="card-title">Estado</h8>
+                                    <h6 class="card-text">{this.props.mensaje.confirmed_1}-CONFIRMADO</h6>
                                 </div>
                                 </div>
                                 </div>
                             </div>
-                            <div className="col-lg-6">
+                            <div className="col-lg-4">
                                 <div className="bs-component">
                                 <div class="card border-dark mb-3" styles="max-width: 20rem;">
                                 <div class="card-body">
                                     <h8 class="card-title">Partida</h8>
-                                    <h6 class="card-text">Buenos Aires, Puerto Madero</h6>
+                                    <h6>Lola mora....</h6> 
                                     <h8 class="card-title">Destino</h8>
-                                    <h6 class="card-text">Forever</h6>
+                                    <h6 class="card-text">Hyderabad, India</h6>
+                                </div>
                                 </div>
                                 </div>
                             </div>
-                        </div>
+                            <div className="col-lg-4">
+                                <div className="bs-component">
+                                <div class="card border-dark mb-3" styles="max-width: 20rem;">
+                                <div class="card-body">
+                                    <h8 class="card-title">Horario</h8>
+                                    <h6>19:00</h6> 
+                                </div>
+                                </div>
+                                </div>
+                            </div>
                         </div>
                         </div>
                     </div>
--- src/components/canvas.js
+++ src/components/canvas.js
@@ -12,20 +12,20 @@ class Canvas extends React.Component {
       ctx.font = "20px Courier";
       ctx.fillText(this.props.text.name, 90, 127);   // name
       ctx.fillText("Argentina", 90, 161);       // origin
-      ctx.fillText("21-DIC-2021", 340, 161);    // date from
+      ctx.fillText("21-ENE-2022", 340, 161);    // date from
       ctx.fillText("India", 90, 193);           // destiny
-      ctx.fillText(this.props.text.category, 340, 193);    // category
+      ctx.fillText("Invitado", 340, 193);    // category
       ctx.fillText("1", 60, 280);       // gate = 1
       ctx.fillText("777", 180, 280);    // fligh = 777
-      ctx.fillText("20:00", 320, 280);    // boardingtime = 15:12
+      ctx.fillText("19:00", 320, 280);    // boardingtime = 15:12
       ctx.fillText("1A", 430, 280);    // seat...
       /// mini pass
       ctx.fillText(this.props.text.name, 620, 128);   // name
       ctx.fillText("Argentina", 620, 160);       // origin
       ctx.fillText("India", 620, 192);           // destiny
-      ctx.fillText("21DIC21", 565, 250);    // date from
-      ctx.fillText("20:00", 655, 250);    // time
-      //ctx.fillText("20:00", 655, 250);    // Class
+      ctx.fillText("21ENE22", 565, 250);    // date from
+      ctx.fillText("19:00", 655, 250);    // time
+      ctx.fillText("INV", 725, 250);    // Class
       ctx.fillText("1", 585, 290);    // gate
       ctx.fillText("777", 655, 290);    // flight
       ctx.fillText("1A", 730, 290);    // seat
